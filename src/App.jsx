d2352f8666208d3f83c41b207ff3aeeb09bd0ac7
Martin <26260699+martinflischman@users.noreply.github.com>
2026-08-19 13:55:40 +0200

    feat: add email contact button
    - Create the EmailButton component
    - Add an email action to the contact card
    - Style the button with an envelope icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ProfileImage from "./components/ProfileImage";
 import ProfileInfo from "./components/ProfileInfo";
+import EmailButton from "./components/EmailButton";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
         <ProfileImage />
         <div className="card-body items-center text-center">
           <ProfileInfo />
+          <EmailButton />
         </div>
       </div>
     </div>
